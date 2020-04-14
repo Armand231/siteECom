2638c96d4cff42fd9759f0342288990d718458ae
lisapaitschin <47555727+lisapaitschin@users.noreply.github.com>
2020-04-14 14:42:32 +0200

    Add files via upload
    VERSION AVEC POSSIBILITE DE SE CONNECTER EN TANT QUE VENDEUR ET POSTER ANNONCE AVEC IMAGE

--- verifVendeur.php
+++ verifVendeur.php
@@ -34,11 +34,11 @@ if (mysqli_num_rows($result) !== 0) {
 	session_start();
         $_SESSION['email'] = $email;
         $_SESSION['pseudo'] = $pseudo;
-        echo 'Vous êtes connecté !';
+        echo 'Vous êtes connecté vous allez etre redirigé dans quelques secondes!';
 $_SESSION['favcolor'] = 'green';
 $_SESSION['animal']   = 'cat';
 $_SESSION['time']     = time(); ?>
-<meta http-equiv="refresh" content="5; URL=localhost/addItem.html">
+<meta http-equiv="refresh" content="5; URL=ajouterItem.html">
 
 
 <?php
